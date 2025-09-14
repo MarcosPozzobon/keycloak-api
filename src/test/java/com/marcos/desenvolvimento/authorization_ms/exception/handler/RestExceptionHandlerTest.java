57876999692d89487019f7c7b1faf5228efa18ba
MarcosPozzobon <marcosklann13@gmail.com>
2025-09-14 18:51:05 -0300

    wip: adicionado handler generico para erro no keycloak

--- src/test/java/com/marcos/desenvolvimento/authorization_ms/exception/handler/RestExceptionHandlerTest.java
+++ src/test/java/com/marcos/desenvolvimento/authorization_ms/exception/handler/RestExceptionHandlerTest.java
@@ -1,6 +1,7 @@
 package com.marcos.desenvolvimento.authorization_ms.exception.handler;
 
 
+import com.marcos.desenvolvimento.authorization_ms.exception.GenericKeycloakException;
 import com.marcos.desenvolvimento.authorization_ms.exception.UsuarioExistenteException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -21,4 +22,17 @@ class RestExceptionHandlerTest {
         Assertions.assertThat(response.getTitulo()).isEqualTo("Conflict");
         Assertions.assertThat(response.getTimestamp()).isNotNull();
     }
+
+    @Test
+    void deveRetornarExceptionFiltersQuandoGenericKeycloakExceptionForLancada() {
+        GenericKeycloakException ex = new GenericKeycloakException("Usuário já existe");
+
+        ExceptionFilters response = handler.handleGenericKeycloakException(ex);
+
+        Assertions.assertThat(response).isNotNull();
+        Assertions.assertThat(response.getStatus()).isEqualTo(400);
+        Assertions.assertThat(response.getTitulo()).isEqualTo("Bad request");
+        Assertions.assertThat(response.getTimestamp()).isNotNull();
+    }
+
 }
